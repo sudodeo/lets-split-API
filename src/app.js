193b5631d7a82da8637dd7c94107738c1bd98cce
sudodeo <adesolaadeoluwa@gmail.com>
2023-12-25 15:55:39 +0100

    add swagger specifications for project

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express"),
-  routes = require("./routes"),
   helmet = require("helmet"),
   cors = require("cors"),
   morgan = require("morgan"),
   compression = require("compression"),
-  responseTime = require("response-time");
+  responseTime = require("response-time"),
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express"),
+  routes = require("./routes");
 
 require("./database");
 
@@ -15,6 +17,7 @@ app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(responseTime());
+
 if (process.env.NODE_ENV === "dev") {
   app.use(morgan("dev"));
 } else if (process.env.NODE_ENV === "prod") {
@@ -22,6 +25,36 @@ if (process.env.NODE_ENV === "dev") {
 }
 
 app.use("/api/auth", routes.authRoutes);
+app.use("/api/health", routes.healthRoute);
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Lets Split API",
+      version: "1.0.0",
+      description:
+        "API for an expense splitting app designed to simplify and streamline the process of splitting expenses among friends, family, or colleagues.",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Deolu",
+        url: "https://sudodeo.vercel.app",
+        email: "sudodeo@gmail.com",
+      },
+    },
+  },
+  apis: [`${__dirname}/routes/*.route.js`],
+};
+
+const spec = swaggerJsdoc(options);
+
+app.use(
+  "/api/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(spec, { explorer: true })
+);
 
 app.get("/", (req, res) => {
   res.redirect("/api/docs");
