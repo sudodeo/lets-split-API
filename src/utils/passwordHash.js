885624367d71be6569f0005b959fd04a9a7b32a4
sudodeo <adesolaadeoluwa@gmail.com>
2023-12-29 06:05:43 +0100

    create password util

---
+++ src/utils/passwordHash.js
@@ -0,0 +1,13 @@
+const bcrypt = require("bcrypt");
+
+const hashPassword = async (unhashedPassword) => {
+  let salt = await bcrypt.genSalt();
+
+  return await bcrypt.hash(unhashedPassword, salt);
+};
+
+const compareHash = async (newHash, oldHash) => {
+  return await bcrypt.compare(newHash, oldHash);
+};
+
+module.exports = { hashPassword, compareHash };
