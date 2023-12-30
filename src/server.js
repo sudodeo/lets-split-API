39fa9db52e8b6fb8ff04cc42672d594e2738aba1
sudodeo <adesolaadeoluwa@gmail.com>
2023-12-30 23:30:00 +0100

    fix error with PORT import

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = require("./config/index");
+const { PORT } = require("./config/index");
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
