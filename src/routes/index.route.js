f7b1d00f104c7d29e1526f5aac2e7a54e316d623
sudodeo <adesolaadeoluwa@gmail.com>
2024-01-02 00:49:04 +0100

    add file to export all route handlers

---
+++ src/routes/index.route.js
@@ -0,0 +1,11 @@
+const authRoutes = require("./auth.route");
+const healthRoute = require("./health.route");
+const expenseRoutes = require("./expense.route");
+const currenciesRoute = require("./currencies.route");
+
+module.exports = {
+  authRoutes,
+  healthRoute,
+  expenseRoutes,
+  currenciesRoute
+};
