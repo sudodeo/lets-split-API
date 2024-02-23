209df6ca93ff19399a0451992add6fb187125a2f
sudodeo <adesolaadeoluwa@gmail.com>
2024-02-24 00:14:09 +0100

    modify database variables

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,9 +1,20 @@
-import { DB_USER, DB_NAME, DB_PASSWORD, DB_PORT, DB_HOST } from "./index.js";
+import {
+  DB_USER,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_HOST,
+  NODE_ENV,
+  DB_CONN_STRING,
+} from "./index.js";
+let connectionString;
+
+if (NODE_ENV == "dev") {
+  connectionString = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+} else if (NODE_ENV === "prod") {
+  connectionString = DB_CONN_STRING;
+}
 
 export {
-  DB_USER as user,
-  DB_HOST as host,
-  DB_NAME as database,
-  DB_PASSWORD as password,
-  DB_PORT as port,
+  connectionString
 };
