e28e2c6cbb82377272d7e00b716ed0cbcf0753dc
sudodeo <adesolaadeoluwa@gmail.com>
2024-02-28 16:53:01 +0100

    add validation functions

---
+++ src/utils/validator.ts
@@ -0,0 +1,141 @@
+import { ValidationError } from "../types/error.type";
+import { User } from "../types/user.types";
+
+export const validateRegistration = async (
+  payload: User
+): Promise<ValidationError[]> => {
+  const errors: ValidationError[] = [];
+
+  const { email, firstName, lastName, dob, password } = payload;
+
+  const emailErrors = validateEmail(email);
+  if (emailErrors.length > 0) {
+    errors.push(...emailErrors);
+  }
+
+  if (firstName == null || firstName === "") {
+    errors.push({
+      field: "firstName",
+      error: "first name is required",
+    });
+  }
+
+  if (lastName == null || lastName === "") {
+    errors.push({
+      field: "lastName",
+      error: "last name is required",
+    });
+  }
+
+  if (dob == null) {
+    errors.push({
+      field: "dob",
+      error: "date of birth is required",
+    });
+    return errors;
+  }
+
+  const dobErrors = validateDob(dob);
+  if (dobErrors.length > 0) {
+    errors.push(...dobErrors);
+  }
+
+  if (password == null || password === "") {
+    errors.push({
+      field: "password",
+      error: "password is required",
+    });
+  }
+
+  const passwordErrors = validatePassword(password);
+  if (passwordErrors.length > 0) {
+    errors.push(...passwordErrors);
+  }
+
+  return errors;
+};
+
+export function validateEmail(email: string): ValidationError[] {
+  const errors: ValidationError[] = [];
+  if (email == null || email == "") {
+    errors.push({
+      field: "email",
+      error: "email is required",
+    });
+    return errors;
+  }
+
+  const emailRegex = /S+@S+.S+/;
+  if (!emailRegex.test(email)) {
+    errors.push({
+      field: "email",
+      error: "invalid email address",
+    });
+  }
+
+  return errors;
+}
+
+export function validateDob(dob: Date): ValidationError[] {
+  const errors: ValidationError[] = [];
+
+  const currentYear = new Date().getFullYear();
+  const dobYear = dob.getFullYear();
+  if (currentYear - dobYear < 18) {
+    errors.push({
+      field: "dob",
+      error: "user must be at least 18 years old",
+    });
+  }
+
+  // invalid date
+  if (isNaN(dob.getTime())) {
+    errors.push({
+      field: "dob",
+      error: "invalid date of birth",
+    });
+  }
+
+  return errors;
+}
+
+export function validatePassword(password: string): ValidationError[] {
+  const errors: ValidationError[] = [];
+
+  if (password.length < 8) {
+    errors.push({
+      field: "password",
+      error: "password must be at least 8 characters long",
+    });
+  }
+
+  if (!/[A-Z]/.test(password)) {
+    errors.push({
+      field: "password",
+      error: "password must contain at least one uppercase letter",
+    });
+  }
+
+  if (!/[a-z]/.test(password)) {
+    errors.push({
+      field: "password",
+      error: "password must contain at least one lowercase letter",
+    });
+  }
+
+  if (!/\d/.test(password)) {
+    errors.push({
+      field: "password",
+      error: "password must contain at least one number",
+    });
+  }
+
+  if (!/[^A-Za-z0-9]/.test(password)) {
+    errors.push({
+      field: "password",
+      error: "password must contain at least one special character",
+    });
+  }
+
+  return errors;
+}
