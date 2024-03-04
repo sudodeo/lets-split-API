cbfb5433ab8bc5b814519cbf4cf8201d504265b4
sudodeo <adesolaadeoluwa@gmail.com>
2024-03-04 01:32:24 +0100

    comment out unused code

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,10 @@
-import userModel from "../models/user.model";
-import { UpdateUser } from "../types/user.types";
+// import userModel from "../models/user.model";
+// import { UpdateUser } from "../types/user.types";
 
-const updateUserProfile = async (newProfileData: UpdateUser) => {
-  return userModel.updateUser(newProfileData);
-};
+// const updateUserProfile = async (newProfileData: UpdateUser) => {
+//   return userModel.updateUser(newProfileData);
+// };
 
-export default {
-  updateUserProfile,
-};
+// export default {
+//   updateUserProfile,
+// };
