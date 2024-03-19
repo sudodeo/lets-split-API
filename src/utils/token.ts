9e5b19d88a11de761b2422a95eadf946f1080ba9
sudodeo <adesolaadeoluwa@gmail.com>
2024-03-19 03:17:39 +0100

    move token logic to new file

---
+++ src/utils/token.ts
@@ -0,0 +1,40 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import crypto from "crypto";
+import { JWT_SECRET } from "../config";
+import logger from "../config/loggerConfig";
+
+export const generateJwt = (id: string, role: string) => {
+  const maxAge = 5 * 60 * 1000; // 5 hours
+  return jwt.sign(
+    {
+      sub: id,
+      iat: new Date().getTime(),
+      iss: process.env.JWT_ISS as string,
+      aud: process.env.JWT_AUD as string,
+      role
+    },
+    JWT_SECRET as string,
+    { expiresIn: maxAge }
+  );
+};
+
+export const verifyJwt = (token: string): JwtPayload => {
+  try {
+    return jwt.verify(token, JWT_SECRET as string, {
+      complete: true,
+    });
+  } catch (error) {
+    logger.error(`verifyJwt error: ${error}`);
+    throw error;
+  }
+};
+
+export const refreshJwt = (_token: string) => {
+  // TODO
+};
+
+export const generateToken = async () => {
+  const token = crypto.randomBytes(32).toString("hex");
+  // const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
+  return token;
+};
