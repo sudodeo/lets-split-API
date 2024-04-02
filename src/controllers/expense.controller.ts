eec2de18ed8c6b7eeb15293c26d2a6c812d2e160
sudodeo <adesolaadeoluwa@gmail.com>
2024-04-02 20:55:39 +0100

    switch to class-based approach

--- src/controllers/expense.controller.ts
+++ src/controllers/expense.controller.ts
@@ -1,92 +1,80 @@
 import logger from "../config/loggerConfig";
-import expenseModel from "../models/expense.model";
-import currencyModel from "../models/currencies.model";
-import { NextFunction, Request, Response } from "express";
-import { BadRequest, Unauthorized } from "../middleware/error.middleware";
+import { NextFunction, Response } from "express";
+import {
+  HttpCode,
+  ResourceNotFound,
+  Unauthorized,
+} from "../middleware/error.middleware";
+import { ExpenseService } from "../services/expense.service";
+import { AuthenticatedRequest } from "../middleware/auth.middleware";
 
-const getAllExpenses = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  const { created } = req.query;
+export class ExpenseController {
+  private expenseService = new ExpenseService();
 
-  const userID = req.authUser?.id;
+  listExpenses = async (
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      const userID = req.authUser?.id;
 
-  if (userID == null) {
-    throw new Unauthorized("you do not have permission to perform this action");
-  }
+      if (userID == null) {
+        throw new Unauthorized(
+          "you do not have permission to perform this action",
+        );
+      }
 
-  let expenses = [];
-  try {
-    if (created === "true") {
-      expenses = await expenseModel.getCreatedExpenses(userID);
-    } else {
-      expenses = await expenseModel.getAllExpenses(userID);
+      const expenses = await this.expenseService.listExpenses(
+        req.query,
+        userID,
+      );
+
+      res.status(HttpCode.OK).json({ success: true, expenses });
+    } catch (error) {
+      logger.error(`listExpenses error: ${error}`);
+
+      next(error);
     }
+  };
 
-    res.status(200).json({ success: true, expenses });
-  } catch (error) {
-    logger.error(`getAllExpenses error: ${error}`);
+  getExpense = async (
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      const { id } = req.params;
+      const expense = await this.expenseService.getExpense(id);
+      if (!expense) {
+        throw new ResourceNotFound("expense not found");
+      }
 
-    next(error);
-  }
-};
-
-const getExpense = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  try {
-    const { expenseID } = req.params;
-    const expense = await expenseModel.getExpense(expenseID);
-    if (!expense) {
-      res.status(404).json({ success: false, error: "expense not found" });
-      return;
+      res.status(HttpCode.OK).json({ success: true, expense });
+    } catch (error) {
+      logger.error(`getExpense error: ${error}`);
+      next(error);
     }
+  };
 
-    res.status(200).json({ success: true, expense });
-  } catch (error) {
-    logger.error(`getExpense error: ${error}`);
+  createExpense = async (
+    req: AuthenticatedRequest,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const expense = await this.expenseService.createExpense(req.body);
+      if (!expense) {
+        throw new Error("expense not created");
+      }
 
-    next(error);
-  }
-};
-
-const createExpense = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    // Check if the currency is supported
-    const currency = await currencyModel.getCurrency(req.body.currency_code);
-    if (!currency || !currency.is_active) {
-      throw new BadRequest("Currency not supported");
+      res.status(HttpCode.CREATED).json({ success: true, expense });
+    } catch (error) {
+      logger.error(`createExpense error: ${error}`);
+      next(error);
     }
+  };
 
-    req.body.currency_code_id = currency.id;
-
-    const expense = await expenseModel.createExpense(req.body);
-    if (!expense) {
-      throw new Error("expense not created");
-    }
-
-    res.status(201).json({ success: true, expense });
-  } catch (error) {
-    logger.error(`createExpense error: ${error}`);
-    next(error);
-  }
-};
-
-const getExpenseSummary = async (_req: Request, _res: Response) => {};
-const settleExpense = async (_req: Request, _res: Response) => {};
-
-export default {
-  getAllExpenses,
-  getExpense,
-  getExpenseSummary,
-  createExpense,
-  settleExpense,
-};
+  // getExpenseSummary = async (_req: Request, _res: Response) => {};
+  // settleExpense = async (_req: Request, _res: Response) => {};
+}
